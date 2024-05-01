06b49a8ee1da0f01c17c8c4902d48f653e5adb34
Rick T. Zhang <rtzhang@ucsb.edu>
2024-05-01 02:26:12 +0000

    rt.z - UCSB Dining Commons Menu Item Controller Functionality (tested)

--- src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonsMenuItemsRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonsMenuItemsRepository.java
@@ -1,12 +1,12 @@
 package edu.ucsb.cs156.example.repositories;
 
-import edu.ucsb.cs156.example.entities.UCSBDiningCommonsMenuItems;
+import edu.ucsb.cs156.example.entities.UCSBDiningCommonsMenuItem;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface UCSBDiningCommonsMenuItemsRepository extends CrudRepository<UCSBDiningCommonsMenuItems, Long> {
-  Iterable<UCSBDiningCommonsMenuItems> findAllByDiningCommonsCode(String diningCommonsCode);
+public interface UCSBDiningCommonsMenuItemsRepository extends CrudRepository<UCSBDiningCommonsMenuItem, Long> {
+  Iterable<UCSBDiningCommonsMenuItem> findAllByDiningCommonsCode(String diningCommonsCode);
 }
