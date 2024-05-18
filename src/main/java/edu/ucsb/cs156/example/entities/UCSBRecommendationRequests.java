8db76392ef3b94ac42eba1e7bd2979599af8f0d1
Shuhan Lyu <shuhanlyu@ucsb.edu>
2024-05-17 17:05:52 -0700

    done

--- src/main/java/edu/ucsb/cs156/example/entities/UCSBRecommendationRequests.java
+++ src/main/java/edu/ucsb/cs156/example/entities/UCSBRecommendationRequests.java
@@ -2,6 +2,7 @@ package edu.ucsb.cs156.example.entities;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
@@ -20,12 +21,19 @@ import lombok.Builder;
 public class UCSBRecommendationRequests {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "ID")
   private long id;
-
+  
+  @Column(name = "REQUESTEREMAIL")
   private String requesterEmail;
+  @Column(name = "PROFESSOREMAIL")
   private String professorEmail;
+  @Column(name = "EXPLANATION")
   private String explanation;
+  @Column(name = "DATEREQUESTED")
   private LocalDateTime dateRequested;
+  @Column(name = "DATENEEDED")
   private LocalDateTime dateNeeded;
+  @Column(name = "DONE")
   private boolean done;
 }
